1e5e70a87ce3c9b7b6e95f565d9bfd96ef4e7d97
wilzh40 <wilzh40@gmail.com>
2016-01-27 01:39:13 -0500

    Now displays a heatmap around local area ~200 miles

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-var myLatlng = new google.maps.LatLng(48.3333, 16.35);
-// sorry - this demo is a beta
-// there is lots of work todo
-// but I don't have enough time for eg redrawing on dragrelease right now
-var myOptions = {
-    zoom: 2,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: false,
-    scrollwheel: true,
-    draggable: true,
-    navigationControl: true,
-    mapTypeControl: false,
-    scaleControl: true,
-    disableDoubleClickZoom: false
-};
-map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
-
-heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
 
 document.getElementById("gen").onclick = function(){
     var x = 5;
@@ -40,9 +21,6 @@ var testData={
 
 
 // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
-google.maps.event.addListenerOnce(map, "idle", function(){
-    heatmap.setDataSet(testData);
-});
 
 function getEvents(lat, lon) {
     jQuery.get( "https://www.eventbriteapi.com/v3/events/search/?location.within=200mi&location.latitude="+ lat +"&location.longitude="+ lon +"&token=YRKRMUBSHY35H3KOZ5UX", function( response  ) {
@@ -50,58 +28,60 @@ function getEvents(lat, lon) {
         // var count = response.pagination.page_count;
         count = 50;
         console.log(count);
+        loadMap();
         while (count--){
             var id = response.events[count].id;
             if (response.events[count].hasOwnProperty('venue_id')) {
                 var venue_id = response.events[count].venue_id;
-                jQuery.get( "https://www.eventbriteapi.com/v3/venues/" + venue_id + "/?token=DBYBJ4BPXUHICOJOSQUC", function( response  ) {
+                var p_token = "RPBLEBK4KZZEJ6TFRPRC"
+                jQuery.get( "https://www.eventbriteapi.com/v3/venues/" + venue_id + "/?token=" + p_token, function( response  ) {
                     console.log(response);
+                    var lat = response.latitude;
+                    var lon = response.longitude;
+                    heatmap.addDataPoint(lat,lon,1);
 
-                    });
+                });
 
-                }
-
-
-
+            }
 
         }
-
-
-
-         }  );
+    }  );
 }
 document.getElementById("search-button").addEventListener("click", function(){
     //window.location = document.getElementById('link-box').value;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-            console.log("lol");
+        console.log("lol");
 
 
 
     }
 });
-    function showPosition(position) {
-        var x = document.getElementById("link-box");
+function showPosition(position) {
+    var x = document.getElementById("link-box");
 
-        console.log( "Latitude: " + position.coords.latitude +
+    console.log( "Latitude: " + position.coords.latitude +
                 "<br>Longitude: " + position.coords.longitude);
 
-        getEvents(position.coords.latitude,position.coords.longitude);
-        myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        loadMap();
+                myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                getEvents(position.coords.latitude,position.coords.longitude);
 
-    }
+}
+ var testData={
+        max: 46,
+        data: [{lat: 33.5363, lng:-117.044, count: 1},{lat: 33.5608, lng:-117.24, count: 1}]
+    };
 function loadMap(){
 
     // sorry - this demo is a beta
     // there is lots of work todo
     // but I don't have enough time for eg redrawing on dragrelease right now
     var myOptions = {
-        zoom: 2,
+        zoom: 10,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         disableDefaultUI: false,
-        scrollwheel: true,
+        scrollwheel: false,
         draggable: true,
         navigationControl: true,
         mapTypeControl: false,
@@ -122,22 +102,24 @@ function loadMap(){
 
             heatmap.addDataPoint(lat,lng,count);
 
-		}
+        }
 
-	};
+    };
 
-	document.getElementById("tog").onclick = function(){
-		heatmap.toggle();
-	};
-
-	var testData={
-    		max: 46,
-    		data: [{lat: 33.5363, lng:-117.044, count: 1},{lat: 33.5608, lng:-117.24, count: 1}]
-    	};
+    document.getElementById("tog").onclick = function(){
+        heatmap.toggle();
+    };
 
 
-	// this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
-	google.maps.event.addListenerOnce(map, "idle", function(){
-		heatmap.setDataSet(testData);
-	});
+
+    // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
 };
+    google.maps.event.addListenerOnce(map, "idle", function(){
+        heatmap.setDataSet(testData);
+    });
+    google.maps.event.addListenerOnce(map, "zoom_changed", function(){
+        heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
+    });
+    google.maps.event.addListenerOnce(map, "zoom_changed", function(){
+        heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
+    });
